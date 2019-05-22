981a7faca309b9c2128212dda18606f05636f35e
Tatsuki Yamamoto <yama.tatsuki@jp.fujitsu.com>
2019-05-22 16:54:58 +0900

    History モード オフ

--- src/router.js
+++ src/router.js
@@ -7,7 +7,7 @@ import CompleteArea from './views/CompleteArea.vue'
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',
+  // mode: 'history',
   base: process.env.BASE_URL,
   routes: [
     {
